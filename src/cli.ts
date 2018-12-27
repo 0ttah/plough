03caf1cb1ebe3d9a13dc741e97226842e1d495d4
Theo Ottah <357850+ottah@users.noreply.github.com>
2018-12-27 07:05:49 +0000

    command directory

--- src/cli.ts
+++ src/cli.ts
@@ -1,74 +1,14 @@
 #!/usr/bin/env node
 import colors from "colors";
 import yargs from "yargs";
-import downloadCommandHandler from "./commands/download";
 import { transformCommandHandler } from "./commands/transform/index";
 const yargv = yargs
-  .command(["transform", "t"], "Transform card set", {
-    script: {
-      alias: "s",
-      required: true,
-    },
-  },
-    (argv) => {
-      console.log(argv);
-      transformCommandHandler(argv);
-    })
-  .command(["download", "d"], "Download card set", {
-    output: {
-      alias: "o",
-      default: "./assets/",
-      describe: "Output location",
-      type: "string",
-    },
-    pictures: {
-      alias: "p",
-      default: false,
-      describe: "Download all images for cards",
-      type: "boolean",
-    },
-    sets: {
-      alias: "s",
-      array: true,
-      default: [0, 1],
-      describe: "Card sets to download",
-    },
-    redownload: {
-      default: false,
-      describe: "Redownload images if they exist on the filesystem.",
-      alias: "r",
-      type: "boolean",
-    },
-    fragment: {
-      default: false,
-      describe: "Split cards into JSON fragments and place them into card folders.",
-      alias: "f",
-      type: "boolean",
-    },
-    wipe: {
-      default: false,
-      describe: "Wipe all files from output directory?",
-    },
-  }, (argv) => {
-    const sets: number[] = argv.s;
-    const valid = sets.every((x) => typeof x === "number");
-    if (!valid) {
-      console.log(colors.red.bold.underline("Sets must be numbers!"));
-      console.log("plough download --help");
-      return valid;
-    }
-    downloadCommandHandler(argv);
-  })
+  .commandDir("commands")
+  .demandCommand()
   .showHelpOnFail(true)
   .example("d", "plough download -p -s 0 1 -o ./artifact/assets")
   .alias("h", "help")
   .alias("v", "version")
-  .option("language", {
-    alias: "l",
-    type: "string",
-    default: "english",
-    describe: "Language of card.",
-  })
   .version()
   .help()
   .argv;
---
+++ src/commands/download/download.ts
@@ -0,0 +1,83 @@
+import axios from "axios";
+import colors from "colors";
+import { emoji } from "node-emoji";
+import yargs = require("yargs");
+import { removeFolder, transformToJSON } from "../../functions";
+import downloadSet from "../../functions/downloadSet";
+
+export const command = ["download", "d"];
+export const desc = "Download card sets";
+export const builder = {
+  output: {
+    alias: "o",
+    default: "./assets/",
+    describe: "Output location",
+    type: "string",
+  },
+  pictures: {
+    alias: "p",
+    default: false,
+    describe: "Download all images for cards",
+    type: "boolean",
+  },
+  sets: {
+    alias: "s",
+    array: true,
+    default: [0, 1],
+    describe: "Card sets to download",
+  },
+  redownload: {
+    default: false,
+    describe: "Redownload images if they exist on the filesystem.",
+    alias: "r",
+    type: "boolean",
+  },
+  fragment: {
+    default: false,
+    describe: "Split cards into JSON fragments and place them into card folders.",
+    alias: "f",
+    type: "boolean",
+  },
+  wipe: {
+    default: false,
+    describe: "Wipe all files from output directory?",
+  },
+  language: {
+    alias: "l",
+    type: "string",
+    default: "english",
+    describe: "Language of card.",
+  },
+};
+
+export function handler(argv: yargs.Arguments): void {
+  const valid = argv.sets.every((x: any) => typeof x === "number");
+  if (!valid) {
+    console.log(colors.red.bold.underline("Sets must be numbers!"));
+    console.log("plough download --help");
+    yargs.exitProcess(true);
+  }
+
+  const setIds: number[] = argv.s;
+  const artifactAPI = axios.create({
+    baseURL: "https://playartifact.com/cardset/",
+    method: "get",
+    transformResponse: [transformToJSON],
+  });
+  console.log("Ploughing", emoji.tractor);
+  // Wipe folder if set
+  if (argv.wipe) {
+    const outputPath = argv.output;
+    console.log(`Wiping: ${outputPath}`);
+    removeFolder(outputPath);
+  }
+  // Wait for sets to be downloaded
+  const sets = setIds.map(async (id) => downloadSet(id, artifactAPI, argv));
+  Promise
+    .all(sets)
+    .then((values) => {
+      const msg = "Finished ploughing";
+      console.log(msg, emoji.tractor);
+    });
+
+}
