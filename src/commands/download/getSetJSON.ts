25abaf9988c56a920de5666ef02a142391c47dbd
Theo Ottah <357850+ottah@users.noreply.github.com>
2018-12-28 21:17:03 +0000

    Fixed missing call to transformToJSON

--- src/commands/download/getSetJSON.ts
+++ src/commands/download/getSetJSON.ts
@@ -1,13 +1,12 @@
 import { CardAPIObject } from "@open-artifact/api-types";
 import axios from "axios";
 import CDN from "./CDN";
-import { transformToJSON } from "./transformToJSON";
 // get set json
 export async function getSetJSON(cdn: CDN): Promise<CardAPIObject> {
   console.log("Retrieving set " + cdn.setId, cdn.fullURL);
   return axios
     .get(cdn.fullURL, {
-      transformResponse: [transformToJSON],
+      transformResponse: (data: string) => JSON.parse(data),
     })
     .then((response) => response.data);
 }
